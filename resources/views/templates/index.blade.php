457c6ad643e8a0a923d3ca21dc2eeabb0b1e67be
Laura <lauralekeu@gmail.com>
2021-02-18 15:39:45 +0100

    Intégration du template
    - Découpe du template en partials
    - Instance de Vue

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		@include('templates.partials._head')
+	</head>
+	<body>
+		
+		<div id="app"></div>
+
+		@include('templates.partials._scripts')
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('templates.index');
 });
 
 Route::get('/dashboard', function () {
