06a79ea73e7303e9d4abf991b51c57bc3fa18d5a
romainIEPS <romain.garcia@hotmail.com>
2021-02-20 16:00:44 +0100

    Mise en place de l'API Resources et Categories

--- app/Http/Controllers/Resources.php
+++ app/Http/Controllers/Resources.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Resource;
 
 class Resources extends Controller
-{
-    //
+{   
+    /**
+     * Renvoi de de toute les ressources au format JSON
+     *
+     * @return json
+     */
+    public function index() {
+        return response()->json(Resource::all());
+    }
 }
